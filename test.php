1eac95c39a9dcaedc257b9bb844cb01b373332fd
Saeed Mohtasham <saeed.najdmohtasham@gmail.com>
2016-06-12 16:18:39 +0200

    Add files via upload

---
+++ test.php
@@ -0,0 +1,36 @@
+<?php
+$servername = "localhost";
+$username = "saeed";
+$password = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully".  "<br>".  "<br>";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SHOW DATABASES;";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "database: " . $row["Database"].  "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
+
+?>
