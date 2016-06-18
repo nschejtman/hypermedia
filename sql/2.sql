aff04c0d38aa463b1dc075cbf25ad38a6c2586d9
Saeed Mohtasham <saeed.najdmohtasham@gmail.com>
2016-06-18 18:45:14 +0200

    Add files via upload

---
+++ sql/2.sql
@@ -0,0 +1,23 @@
+INSERT INTO `products` (`id`, `name`, `category`, `description`, `price`, `brand`) VALUES ('21', 'Samsung Smart TV 50"', 'tv', 'Full HD 1080p
+Enjoy a viewing experience that is 2X the clarity of standard HD TVs.', '750', 'samsung');
+
+INSERT INTO `products` (`id`, `name`, `category`, `description`, `price`, `brand`) VALUES ('22', 'Samsung Smart TV 48"', 'tv', '48" H6400 6 Series Flat Full HD Smart 3D LED TV', '720', 'samsung');
+
+INSERT INTO `products` (`id`, `name`, `category`, `description`, `price`, `brand`) VALUES ('23', 'Decoder TIMvision', 'decoder', 'Thousands of titles including cartoons, movies, series and documentaries.', '49', 'TIM');
+
+INSERT INTO `products` (`id`, `name`, `category`, `description`, `price`, `brand`) VALUES ('24', 'Nokia speakers MD12', 'speaker', 'Listening to your favorite music is now easier.With NFC included Up to 15 hours of music playback', '49.90', 'Nokia');
+
+INSERT INTO `products` (`id`, `name`, `category`, `description`, `price`, `brand`) VALUES ('25', 'Cuffie Beats «Solo2»', 'headphone', 'Audio enhanced with the new Beats sound quality, the sound of Solo2 headphones is sharper and cleaner and emits a broader range of emotions', '199.90', 'beats');	
+
+INSERT INTO `products` (`id`, `name`, `category`, `description`, `price`, `brand`) VALUES ('26', 'Samsung Galaxy Gear S', 'sensor', 'An elegant curved Super AMOLED display for optimal fit, customizable screens and interchangeable straps to better express their own style', '349.90', 'Samsung');
+
+INSERT INTO `products` (`id`, `name`, `category`, `description`, `price`, `brand`) VALUES ('27', 'Polar Loop H7 HR', 'sensor', 'In conjunction with Polar Heart Rate Sensor. For those who want to measure their business 24 hours a 24 and be driven to achieve daily goals', '99.90', 'Polar');
+
+INSERT INTO `products` (`id`, `name`, `category`, `description`, `price`, `brand`) VALUES ('28', 'Polar HR', 'sensor', 'Used with a mobile application for sports or with a compatible Polar training computer, the sensor displays in an extremely precise real-time heart rate during training', '69.90', 'Polar');
+
+INSERT INTO `products` (`id`, `name`, `category`, `description`, `price`, `brand`) VALUES ('29', 'iHealth HS5', 'sensor', 'Record the calories and daily activities, plans weight goals. Organize files, graphics, the historian of the readings and displays trends', '119.90', 'iHealth');
+
+INSERT INTO `products` (`id`, `name`, `category`, `description`, `price`, `brand`) VALUES ('30', 'Sony Smartband FE', 'sensor', 'An innovative wrist band can automatically record your physical, social activities and entertainment', '49.90', 'Sony');
+
+
+
