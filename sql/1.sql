3fb1204c492733c4ed933a628345e5e9602f995a
Amin Fathi <amin.fathi65@icloud.com>
2016-06-18 12:16:00 +0200

    Update 1.sql
    Extended productId from 12 to 20 by Amin

--- sql/1.sql
+++ sql/1.sql
@@ -53,7 +53,53 @@ INSERT INTO `products` (`id`, `name`, `category`, `description`, `price`, `brand
     with 2.7 GHz speed. Keep going all day and night with Turbo charging and up to 48-hour battery life. See every
     detail with the 5.2-inch Quad HD display and 21-megapixel camera. With Ballistic Nylon, it''s as strong as it is
     quick. The Droid Turbo comes in 32GB or 64GB. Available in Black Ballistic Nylon.',
-   199.90, 'Motorola');
+   199.90, 'Motorola'),
+   
+   
+   (12, 'Huawei P9 Plus', 'smartphone', 'The experience of Huawei P9 is elevated to a higher level with Huawei P9 Plus 
+	  thanks to the bright display of 5.5 "and the stylish metal body.Thanks to the perfect combination of hardware 
+	  and superior software dual Leica camera, P9 Plus lets you capture the highest quality images without sacrificing 
+	  the compact and stylish design.',
+    749.90, 'Huawei'),
+(13, 'LG G5', 'smartphone', 'The new LG G5 is a new type of smartphone that exceeds all limits , with a modular design
+	that revolutionizes the metal fully specified design standards. Expand the functions , interact with the 
+	innovative LG Friends and test truly innovative smartphone experience. The G5 revolution continues with 
+	a double wide-angle camera, which captures more than your eyes can see.',
+    699.90, 'LG'),
+(14, 'Sony XPERIA X', 'smartphone', 'A smartphone with inimitable performance that integrates a stunning camera that 
+	can predict the next movement of the subject. Xperia X Performance integrates the new camera Sony signed, faster 
+	and smarter than ever. Faster response, focus more intelligent and revolutionary adaptive shutter speed: you''ll 
+	have everything you need for incredibly sharp shots in all conditions.',
+    629.90, 'Sony'),
+(15, 'Apple iPhone 6S 16GB', 'smartphone', 'Retina HD display, 4.7 "3D Touch. 7000 series aluminum and more resistant
+	glass. A9 chip with 64-bit architecture of desktop level. New iSight camera with 12MP Live Photos. Touch ID. 
+	Wi-Fi connections and 4G LTE more veloci.1 long battery durata.2 and yet, iOS 9 and iCloud. all in a sleek 
+	unibody.',
+    789.90, 'Apple'),
+
+(16, 'Samsung Galaxy TAB A 9.7', 'tablet', 'The essential style and metallic frame enhance the new format 4: 3, 
+	giving the Galaxy TAB In a modern and sophisticated look, ensuring maximum mobility thanks to the 7.5 mm thick 
+	and only 456 grams of weight.Designed to be the most portable tablet category, Galaxy TAB A no compromise, 
+	giving you a wider screen than other tablet.',
+    299.90, 'Samsung'),
+(17, 'Samsung Galaxy TabPro S 12', 'tablet', 'TabPro Galaxy S is the perfect combination of design and technology. 
+	Extremely thin and light, it impresses with refined details and quality of materials.The book cover perfectly 
+	protects Galaxy S TabPro, keeping intact the elegance.TabPro Galaxy S is the first product in the world with 
+	Windows 10 SUPER AMOLED display.',
+    1299.90, 'Samsung'),
+(18, 'Huawei Mediapad T1 10 LTE', 'tablet', 'Including 2GB / month 4G free promotion for the first 3 months',
+    249.90, 'Huawei'),
+
+(19, 'Apple iPad Pro 9.7" 256 GB', 'tablet', 'iPad Pro 9.7 "is a concentration ever of portability and performance. 
+	It is often only 6.1 mm and weighs less than 500 grams, but the Retina display brighter and evolved ever: 
+	with its True Tone sensors adapts to ambient light, so the eyes do not get tired. Plus get powerful 64-bit 
+	desktop chip A9X level audio to four speakers.',
+    1199.90, 'Apple'),
+(20, 'Apple iPad mini 4 16GB', 'tablet', 'To view it seems really small, but is ready to surprise you with outstanding
+	performance: The new iPad mini 4, thinner and lighter than ever, has all the power you need to do great things. 
+	iPad mini 4 contains everything you love the iPad for an even more lightweight, portable design. Call friends 
+	with FaceTime or carried on with the job: you can do it anywhere, anytime.',
+    529.90, 'Apple');
 
 INSERT INTO `product_feature` (`product_id`, `property`, `value`) VALUES
   (3, 'Technology', 'ADSL/ADSL2+'),
@@ -114,6 +160,85 @@ INSERT INTO `product_feature` (`product_id`, `property`, `value`) VALUES
   (8, 'Display', 'Retina display 4.7’’ - 720x1280'),
   (9, 'Display', 'HD Touchscreen LED 4.7’’ - 720x1280'),
   (10, 'Display', 'HD Touchscreen LED 4.7’’ - 720x1280'),
-  (11, 'Display', 'HD Touchscreen SuperAMOLED 4.7’’ - 720x1280')
-;
+  (11, 'Display', 'HD Touchscreen SuperAMOLED 4.7’’ - 720x1280'),
+
+
+(12, 'OS', 'Android'),
+    (13, 'OS', 'Android'),
+    (14, 'OS', 'Android'),
+    (15, 'OS', 'iOS'),
+	
+	(16, 'OS', 'Android'),
+    (17, 'OS', 'Android'),
+    (18, 'OS', 'Android'),
+    (19, 'OS', 'iOS'),
+	(20, 'OS', 'iOS'),
+
+  
+    (12, 'Size', '152.3 x 75.3 x 6.98 mm'),
+    (13, 'Size', '149.4 x 73.9 x 7.7 mm'),
+    (14, 'Size', '142.7 x 69.4 x 7.9 mm'),
+    (15, 'Size', '138.3 x 67.1 x 7.1 mm'),
+
+	(16, 'Size', '242,5 x 166,8 x 7,4 mm'),
+    (17, 'Size', '290.3 x 198.8 x 6.3 mm'),
+    (18, 'Size', '248,5 x 150 x 8,3 mm'),
+    (19, 'Size', '240 x 169.5 x 6.1 mm'),
+	(20, 'Size', '203.2 x 134.8 x 6.1 mm'),
+
+	(12, 'Processor', 'Kirin 955 - Octa-Core (4x2,5 GHz + 4x1,8 GHz)'),
+    (13, 'Processor', 'Snapdragon™ 820 QuadCore 2.15 GHz - 4G LTE'),
+    (14, 'Processor', 'Qualcomm® Snapdragon ™ processor 650 Hexa-Core (1.8 GHz + 1.4 GHz)'),
+    (15, 'Processor', 'A9 chip with integrated coprocessor M9 movement'),
+  
+	(16, 'Processor', 'From quad-core Snapdragon 410 1.2 GHz'),
+    (17, 'Processor', 'Intel Core M3 (Dualcore 2.2 GHz)'),
+    (18, 'Processor', 'Qualcomm MSM8916 Snapdragon 410 1.2 GHz'),
+    (19, 'Processor', 'Chip A9X third-generation 64-bit architecture of desktop level'),
+	(20, 'Processor', 'A8 architecture with 64bit at 1.6GHz , 1GB Ram , co-processor M8'),
+
+    (12, 'Battery', '3400 mAh'),
+    (13, 'Battery', '2800 mAh'),
+    (14, 'Battery', '2620 mAh'),
+    (15, 'Battery', '1715 mAh'),
+
+	(16, 'Battery', '6000 mAh'),
+    (17, 'Battery', '5200 mAh'),
+    (18, 'Battery', '4800 mAh'),
+    (19, 'Battery', 'Li-Ion battery (27.9 Wh)'),
+	(20, 'Battery', '5124 mAh'),
+  
+    (12, 'Connectivity', 'Cat.6 4G / HSPA + / EDGE / GPRS frequencies 850/900 /1800 / 1900Wi - Fi 802.11 a / b / g / n / ac Wi - Fi Direct , Bluetooth 4.2 , NFC , impression sensor , Infrared'),
+    (13, 'Connectivity', '4G cat.6 / HSDPA42 / UMTS / EDGE / GPRS - Wi-Fi - Bluetooth - Infrared - USB Type C - NFC'),
+    (14, 'Connectivity', 'Enabled 4G / HSDPA 42 / UMTS / EDGE / GPRS - Wi-Fi - Bluetooth - Micro USB - NFC'),
+    (15, 'Connectivity', '4G LTE Advanced1 e Wi-Fi 802.11a/b/g/n/ac con tecnologia MIMO'),
+
+	(16, 'Connectivity', 'Wi-Fi 802.11 a/b/g/n - Bluetooth 4.0 - micro USB'),
+    (17, 'Connectivity', 'USB 3.1(C-type) , Bluetooth 4.1 , Wi-Fi 802.11 802.11 a/b/g/n/ac( MIMO ), GPS + GLONASS , NFC , Wi-Fi Direct , 3.5 mm Headset Jack , Nanosim Card'),
+    (18, 'Connectivity', 'LTE, HSDPA+ 42.2Mbps/HSUPA 5.76/, Wi-Fi 802.11b/g/n, Bluetooth 3.0'),
+    (19, 'Connectivity', 'Advanced1 4G LTE , Wi-Fi 802.11ac MIMO'),
+	(20, 'Connectivity', 'LTE /HSDPA-DC 42.2/HSUPA 5.76/, Wi-Fi 802.11a/b/g/n /ac e MIMO, Bluetooth 4.2'),
+   
+    (12, 'Camera', 'Dual 12 MP, f/2.2, 27 mm, Leica optics, phase detection autofocus, dual-LED - Video: 1080p@30fps - Secondary: 8 MP, f/1.9, autofocus, 1080p'),
+    (13, 'Camera', '16 MP (f/1.8) + 8 MP (f/2.4), laser autofocus, OIS (3-axis), LED flash - Video: 2160p@30fps, 1080p@30fps - Secondary: 8 MP, f/2.0, 1080p@30fps'),
+    (14, 'Camera', '23 MP, f/2.0, 24mm, phase detection autofocus, LED flash - Video: 1080p@30fps, 1080p@60fps - Secondary: 13 MP, f/2.0, 22mm'),
+    (15, 'Camera', '12 MP, f/2.2, 29mm, phase detection autofocus, dual-LED - Video: 2160p@30fps, 1080p@60fps,1080p@120fps, 720p@240fps - Secondary: 5 MP, f/2.2, 31mm, 1080p@30fps, 720p@240fps'),
+   
+	(16, 'Camera', '5 MP, autofocus - Video: 720p@30fps - Secondary: 2 MP'),
+    (17, 'Camera', 'Fotocamera (Front: 5 MP Back: 5 MP)'),
+    (18, 'Camera', '2 Camera 5 megapixel and VGA Multimedia, 1080p / 30fps'),
+    (19, 'Camera', '12 MP, f/2.2, 29mm, phase detection autofocus, dual-LED - Video: 2160p@30fps, 1080p@30/60fps, 1080p@120fps, 720p@240fps - Secondary: 5 MP, f/2.2, 31mm, 1080p@30fps, 720p@240fps'),
+	(20, 'Camera', '8 MP, f/2.4 - Video: 1080p@30fps, 720p@120fps - Secondary: 1.2 MP, 720p@30fps'),
+
+    (12, 'Display', 'Full HD Display AMOLED 5.5" - 1080 x 1920'),
+    (13, 'Display', 'IPS LCD  5.3’’ - 1440 x 2560'),
+    (14, 'Display', 'IPS LCD 5.0" - 1080 x 1920'),
+    (15, 'Display', 'LED-backlit IPS LCD 4.7’’ - 750 x 1334'),
+
+	(16, 'Display', 'TFT 9.7" - 768 x 1024'),
+    (17, 'Display', 'Full HD Display 12" - 1440 x 2160'),
+    (18, 'Display', 'IPS LCD 9.6" - 800 x 1280'),
+    (19, 'Display', 'LED-backlit IPS LCD 9.7" - 1536 x 2048'),
+	(20, 'Display', 'Retina Display 7.9" - 1536 x 2048')
+	;
 
