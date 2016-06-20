d0081bad2e9d9c23f696f6f4859b5591fc98ca67
Nicolas Schejtman <nschejtman93@gmail.com>
2016-06-20 19:21:44 +0200

    Fixes on css for navbar and products page

--- products.php
+++ products.php
@@ -127,8 +127,10 @@ require_once 'Data.php';
                 <?php
                 while ($row = $products->fetch_assoc()) {
                     echo '<div class="cell">';
-                    echo '<div class="product"> ';
+                    echo '<div class="product">';
+                    echo '<div class="product-image">';
                     echo '<img src="/images/products/'.$row["id"].'.jpg"> ';
+                    echo '</div>';
                     echo '<span class="name">'. $row["name"].'</span>';
                     echo '<span class="price">'. $row["price"].'</span>';
                     echo '<a class="btn btn-white" href="/products/product.php?pid='.$row["id"].'">Details</a>';
