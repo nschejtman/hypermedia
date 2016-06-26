1d9c23b4b5a525608148d2bc69b861ee98fafd8a
Nicolas Schejtman <nschejtman93@gmail.com>
2016-06-26 15:25:24 +0200

    Started with home.php

--- home.php
+++ home.php
@@ -64,15 +64,98 @@
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
 </nav>
-<div class="container">
+<div id="carousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#carousel" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel" data-slide-to="1"></li>
+        <li data-target="#carousel" data-slide-to="2"></li>
+    </ol>
 
-    <h1> HELLO </h1>
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+        <div class="item active">
+            <img src="/images/fiber.png" alt="fiber">
+            <div class="carousel-caption">
+                <h1>TIM Smart Fiber</h1>
+                <p>TIM Smart Fiber is the complete offer for unlimited, ultrafast browsing. Now with TIM vision
+                    included,
+                    allowing you to watch your favorite TV shows and movies on demand.</p>
+                <a class="pill">Details</a>
+                <br>
+                <br>
+            </div>
+        </div>
+        <div class="item">
+            <img src="/images/samsung-tv-banner.jpg" alt="...">
+            <div class="carousel-caption">
+                <h1>Tim Vision</h1>
+                <p>Cartoons , movies , TV series, documentaries and concerts always on demand
+                    to create your own schedule without advertising . More than 8,000 titles on a subscription ,
+                    with no time constraints , even in HD on TV with dedicated decoder ,
+                    Smart TV , PC , Smartphone and Tablet.
+                </p>
+                <a class="pill">Details</a>
+                <br>
+                <br>
+            </div>
+        </div>
+        <div class="item">
+            <img src="/images/cellphone.jpg" alt="...">
+            <div class="carousel-caption">
+                <h1>Flexible plans</h1>
+                <p>
+                    TIM offers various plans to fit your needs. Prepaid, contract, home internet, mobile internet, on
+                    demand
+                    series, movies, music and games, smart life products and services, the combinations are endless!
+                </p>
+                <a class="pill">Find a plan</a>
+                <br>
+                <br>
+            </div>
+        </div>
+    </div>
+
+    <!-- Controls -->
+    <a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#carousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
 </div>
-
+<div id="help-banner">
+    <div class="container">
+        <h3>Need assitance?</h3>
+        <div class="row">
+            <div class="col-md-4">
+                <h5>Call line 187</h5>
+                <img src="/images/call.png" width="50px" class="center-block">
+            </div>
+            <div class="col-md-4">
+                <h5>General assistance</h5>
+            </div>
+            <div class="col-md-4">
+                <h5>Tech support</h5>
+            </div>
+        </div>
+        <br>
+        <br>
+    </div>
+</div>
+<div id="products-banner">
+    
+</div>
 
 <!-- scripts added last for faster loading -->
 <script src="/bower_components/jquery/dist/jquery.min.js"></script>
 <script src="/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-
+<script src="/scripts/modernizr-custom.js"></script>
+<script>
+    var $carousel = $('#carousel');
+    $carousel.carousel();
+</script>
 </body>
 </html>
